35cf2453ffc3b1de51be2ab43c04adf9aa807812
elton-develcode <elton.develcode@develcode.com.br>
2019-07-25 14:52:17 -0300

    Finalizando o  novo modulo.

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Saudacao from './componentes/Saudacao'
-
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 ReactDOM.render(
   <div>
-    <Saudacao tipo='Saudação' nome='Elton'/>
+    <Pai nome='Elton' sobrenome='Veiga'>
+      <Filho nome='Pedro' />
+      <Filho nome='Jose' />
+      <Filho nome='Paula' />
+    </Pai>
   </div>
 , document.getElementById('root'))
